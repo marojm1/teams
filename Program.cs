fd8931348ae2246e4fa80ce482238dadac1e9c04
Mustafa <162383894+Mustafa12315@users.noreply.github.com>
2024-05-14 15:41:12 +1200

    down

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Team_work
+﻿using System;
+
+namespace Team_work
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, My name is Joanna");
+            Console.WriteLine("Hi, My name is Mustafa");
+            Console.ReadLine();
         }
         
     }
