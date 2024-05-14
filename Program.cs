59914b5f88d0e40542cc8821c24bcee051d91998
Mustafa <162383894+Mustafa12315@users.noreply.github.com>
2024-05-14 15:32:08 +1200

    First

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Team_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+    }
+}
