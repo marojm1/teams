6004a242e99d5c5dd1b9ab0af8d1b9645cbb2111
Thomas Fischer (1000117937) <FISCTJ1@student.op.ac.nz>
2024-05-14 15:43:10 +1200

    done

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Team_work
         {
             Console.WriteLine("Hello, My name is Joanna");
             Console.WriteLine("Hi, My name is Mustafa");
+            Console.WriteLine("Hey My name is Thomas");
             Console.ReadLine();
         }
         
